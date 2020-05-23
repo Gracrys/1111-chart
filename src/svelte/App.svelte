77bbf678f841a6baf0fc131658468bff74b3f1eb
Gracrys <gracrys@gmail.com>
2020-05-22 21:03:34 -0430

    fixed some responsivity

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -2,21 +2,34 @@
 import Genres from './Genres.svelte'
 import Cats from './Cats.svelte'
 import Player from './Player.svelte'
+import menu_icon from '../../assets/icons/menu.svg'
+
+let openState = false
+
+const open = e => openState = !openState
 
 </script>
 
 
-<template type="pug">
- 	aside
-			header
-				h2 Categorias
-				hr.border-indigo-700
-				ul
-					Cats
-		main
-			h1.text-2xl.text-left 1111 Essentials Recordings Of Music v2.00 (2020/04/20)
-			Genres
-			Player
+<template >
+	<button class="menu" on:click="{open}">
+		<img src="{menu_icon }" alt="">
+	</button>
+	<aside class="{openState ? "open": ""}">
+		<header>
+			<h2> Categorias </h2>
+		</header>
+		<hr>
+		<ul>
+			<Cats />
+		</ul>
+	</aside>
+
+	<main>
+		<h1 class="text-2xl text-left "> 1111 Essentials Recordings Of Music v2.00 (2020/04/20) </h1>
+		<Genres />
+		<Player />
+	</main>
 
 </template>
 
